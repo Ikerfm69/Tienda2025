ebee7831caba26e9d3bb76d5fc6f78d2e2fecd64
Iker <Iker@DESKTOP-BJO83TB>
2025-02-18 13:33:52 +0100

    Clases simples.

---
+++ src/main/java/es/educastur/ikerfm/tienda/ComparaArticulosPorPrecio.java
@@ -0,0 +1,12 @@
+package es.educastur.ikerfm.tienda;
+
+import java.util.Comparator;
+
+
+public class ComparaArticulosPorPrecio implements Comparator <Articulo>{
+
+    @Override
+    public int compare(Articulo a1, Articulo a2) {
+        return Double.compare(a1.getPvp(), a2.getPvp());
+    }
+}
